7dda7773f0a77e953f300b804d79d75099509164
happycod3r <happy2code0101@gmail.com>
2023-07-07 15:28:57 -0400

    [feat] added ability to mark a todo item done

--- src/app/components/TodoItem.js
+++ src/app/components/TodoItem.js
@@ -2,25 +2,34 @@
 
 import React from 'react';
 
-const TodoItem = ({ index, todo, deleteTodo, rearrangeTodos }) => {
-    const { text, importance, timestamp } = todo;
+const TodoItem = ({ index, todo, deleteTodo, rearrangeTodos, toggleDone }) => {
+    const { text, importance, timestamp, done } = todo;
     const importanceColors = {
-      low: 'green',
-      medium: 'yellow',
-      high: 'orange',
-      critical: 'red'
+        low: 'green',
+        medium: 'yellow',
+        high: 'orange',
+        critical: 'red'
     };
+
+    const todoItemClass = done ? 'todo-item done' : 'todo-item';
 
     return (
         <li>
-          <span>{text}</span>
-          <span style={{ color: importanceColors[importance] }}> [{importance}]</span>
-          <span> - Created at {timestamp}</span>
-          <button onClick={() => deleteTodo(index)}>Delete</button>
-          <button disabled={index === 0} onClick={() => rearrangeTodos(index, index - 1)}>Move Up</button>
-          <button disabled={index === todo.length - 1} onClick={() => rearrangeTodos(index, index + 1)}>Move Down</button>
+            <span className={todoItemClass}>
+                {text}
+            </span>
+            <span style={{ color: importanceColors[importance] }}> 
+                [{importance}]
+            </span>
+            <span> 
+                - Created at {timestamp}
+            </span>
+            <button onClick={() => deleteTodo(index)}>Delete</button>
+            <button disabled={index === 0} onClick={() => rearrangeTodos(index, index - 1)}>Move Up</button>
+            <button disabled={index === todo.length - 1} onClick={() => rearrangeTodos(index, index + 1)}>Move Down</button>
+            <input type="checkbox" checked={done} onChange={() => toggleDone(index)} />
         </li>
-      );
-    };
+    );
+};
     
-    export default TodoItem;
+export default TodoItem;
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,4 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import styles from './styles/page.module.css'
 import TodoList from './components/TodoList';
 
 export default function Home() {
