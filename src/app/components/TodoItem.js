aa3e296119d67f23b0b72a45604622e1159b85a2
happycod3r <happy2code0101@gmail.com>
2023-07-08 03:14:10 -0400

    [style] Added styling to components. globals.css

--- src/app/components/TodoItem.js
+++ src/app/components/TodoItem.js
@@ -1,33 +1,48 @@
 'use client'
 
 import React from 'react';
+import Image from 'next/image';
+import trashIcon from "../../../public/ui/trash-can-solid.svg";
+import arrowUpIcon from "../../../public/ui/arrow-up-solid.svg";
+import arrowDownIcon from "../../../public/ui/arrow-down-solid.svg";
 
 const TodoItem = ({ index, todo, deleteTodo, rearrangeTodos, toggleDone }) => {
-    const { text, importance, timestamp, done } = todo;
+    const { text, importance, timestamp, done, timeFinished } = todo;
     const importanceColors = {
-        low: 'green',
-        medium: 'yellow',
-        high: 'orange',
-        critical: 'red'
+        low: '#90be6d',
+        medium: '#f9c74f',
+        high: '#f9844a',
+        critical: '#f94144'
     };
 
     const todoItemClass = done ? 'todo-item done' : 'todo-item';
 
     return (
-        <li>
+        <li className='todo-list-item'>
             <span className={todoItemClass}>
                 {text}
             </span>
-            <span style={{ color: importanceColors[importance] }}> 
+            <span className='importance-lvl' style={{ color: importanceColors[importance] }}> 
                 [{importance}]
             </span>
-            <span> 
-                - Created at {timestamp}
+            <span className='created-time-stamp'> 
+                <span className='timestamp-txt'>Created @</span> {timestamp} :
+                {done && timeFinished && (
+                    <span className='done-time-stamp'><span className='timestamp-txt'>Finished @</span> {timeFinished}</span>
+                )}
             </span>
-            <button onClick={() => deleteTodo(index)}>Delete</button>
-            <button disabled={index === 0} onClick={() => rearrangeTodos(index, index - 1)}>Move Up</button>
-            <button disabled={index === todo.length - 1} onClick={() => rearrangeTodos(index, index + 1)}>Move Down</button>
-            <input type="checkbox" checked={done} onChange={() => toggleDone(index)} />
+            <div className='todo-item-ctrls-container'>
+                <button className='delete-todo-btn form-btn' onClick={() => deleteTodo(index)}>
+                    <Image className='icon' src={trashIcon} alt='delete' width={18} height={18}/>
+                </button>
+                <button className='move-todo-up-btn form-btn' disabled={index === 0} onClick={() => rearrangeTodos(index, index - 1)}>
+                    <Image className='icon' src={arrowUpIcon} alt='move up' width={18} height={18}/>
+                </button>
+                <button className='move-todo-down-btn form-btn' disabled={index === todo.length - 1} onClick={() => rearrangeTodos(index, index + 1)}>
+                    <Image className='icon' src={arrowDownIcon} alt='move down' width={18} height={18}/>
+                </button>
+                <input className='toggle-done-checkbox' type="checkbox" checked={done} onChange={() => toggleDone(index)} />
+            </div>
         </li>
     );
 };
--- src/app/components/TodoList.js
+++ src/app/components/TodoList.js
@@ -2,117 +2,133 @@
 
 import React from 'react'; 
 import dynamic from 'next/dynamic'; 
+import Image from 'next/image';
+import filterIcon from '../../../public/ui/filter-solid.svg';
 
 const TodoItem = dynamic(() => import('./TodoItem'), { ssr: false });
 
 class TodoList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      todos: [],
-      filter: 'all'
-    };
-  }
-
-  addTodo = (text, importance) => {
     
-    const timestamp = new Date().toLocaleString();
-    
-    const newTodo = {
-      text: text,
-      importance: importance,
-      timestamp: timestamp,
-      done: false
-    };
+    constructor(props) {
+        super(props);
 
-    this.setState(prevState => ({
-      todos: [...prevState.todos, newTodo]
-    }));
-  }
-
-  deleteTodo = index => {
-    this.setState(prevState => ({
-      todos: prevState.todos.filter((_, i) => i !== index)
-    }));
-  }
-
-  rearrangeTodos = (oldIndex, newIndex) => {
-    this.setState(prevState => {
-      const todos = [...prevState.todos];
-      const [removed] = todos.splice(oldIndex, 1);
-      todos.splice(newIndex, 0, removed);
-      return { todos };
-    });
-  }
-
-  updateFilter = filterValue => {
-    this.setState({ filter: filterValue });
-  }
-
-  filterTodos = () => {
-    const { todos, filter } = this.state;
-    if (filter === 'all') {
-      return todos;
-    } else {
-      return todos.filter(todo => todo.importance === filter);
+        this.state = {
+            todos: [],
+            filter: 'all'
+        };
     }
-  }
 
-  toggleDone = index => {
-    this.setState(prevState => {
-        const todos = [...prevState.todos];
-        todos[index].done = !todos[index].done;
-        return { todos };
-    });
-  }
+    addTodo = (text, importance) => {
+    
+        const timestamp = new Date().toLocaleString();
+    
+        const newTodo = {
+            text: text,
+            importance: importance,
+            timestamp: timestamp,
+            done: false,
+            timeFinished: null
+        };
 
-  render() {
-    const { todos, filter } = this.state;
-    const filteredTodos = this.filterTodos();
+        this.setState(prevState => ({
+            todos: [...prevState.todos, newTodo]
+        }));
+    }
 
-    return (
-      <div>
-        <div>
-          <button onClick={() => this.updateFilter('all')} disabled={filter === 'all'}>All</button>
-          <button onClick={() => this.updateFilter('low')} disabled={filter === 'low'}>Low</button>
-          <button onClick={() => this.updateFilter('medium')} disabled={filter === 'medium'}>Medium</button>
-          <button onClick={() => this.updateFilter('high')} disabled={filter === 'high'}>High</button>
-          <button onClick={() => this.updateFilter('critical')} disabled={filter === 'critical'}>Critical</button>
-        </div>
-        <ul>
-          {filteredTodos.map((todo, index) => (
-            <TodoItem
-              key={index}
-              index={index}
-              todo={todo}
-              deleteTodo={this.deleteTodo}
-              rearrangeTodos={this.rearrangeTodos}
-              toggleDone={this.toggleDone}
-            />
-          ))}
-        </ul>
+    deleteTodo = index => {
+        this.setState(prevState => ({
+            todos: prevState.todos.filter((_, i) => i !== index)
+        }));
+    }
+
+    rearrangeTodos = (oldIndex, newIndex) => {
+        this.setState(prevState => {
+            const todos = [...prevState.todos];
+            const [removed] = todos.splice(oldIndex, 1);
+            todos.splice(newIndex, 0, removed);
+            return { todos };
+        });
+    }
+
+    updateFilter = filterValue => {
+        this.setState({ filter: filterValue });
+    }
+
+    filterTodos = () => {
+        const { todos, filter } = this.state;
+        if (filter === 'all') {
+            return todos;
+        } else if (filter === 'done') {
+            return todos.filter(todo => todo.done);
+        } else {
+            return todos.filter(todo => todo.importance === filter);
+        }
+    }
+
+    toggleDone = index => {
+        this.setState(prevState => {
+            const todos = [...prevState.todos];
+            todos[index].done = !todos[index].done;
+            if (todos[index].done) {
+                todos[index].timeFinished = new Date().toLocaleString();
+            } else {
+                todos[index].timeFinished = null;
+            }
+            return { todos };
+        });
+    }
+
+    render() {
+        const { todos, filter } = this.state;
+        const filteredTodos = this.filterTodos();
+
+        return (
+            <div id='main-conotainer'>
+                <div id='filter-btn-container'>
+                    <span>
+                        <Image className='icon' src={filterIcon} alt='delete' width={18} height={18}/>
+                    </span>
+                    <button id='filter-all-btn' className='filter-btn' onClick={() => this.updateFilter('all')} disabled={filter === 'all'}>All</button>
+                    <button id='filter-low-btn' className='filter-btn' onClick={() => this.updateFilter('low')} disabled={filter === 'low'}>Low</button>
+                    <button id='filter-medium-btn' className='filter-btn' onClick={() => this.updateFilter('medium')} disabled={filter === 'medium'}>Medium</button>
+                    <button id='filter-high-btn' className='filter-btn' onClick={() => this.updateFilter('high')} disabled={filter === 'high'}>High</button>
+                    <button id='filter-critical-btn' className='filter-btn' onClick={() => this.updateFilter('critical')} disabled={filter === 'critical'}>Critical</button>
+                    <button id='filter-done-btn' className='filter-btn' onClick={() => this.updateFilter('done')} disable={filter === 'done'}>Done</button>
+                </div>
+                <ul id='todo-list'>
+                    {filteredTodos.map((todo, index) => (
+                        <TodoItem
+                            key={index}
+                            index={index}
+                            todo={todo}
+                            deleteTodo={this.deleteTodo}
+                            rearrangeTodos={this.rearrangeTodos}
+                            toggleDone={this.toggleDone}
+                        />
+                    ))}
+                </ul>
  
-        <form onSubmit={e => {
-          e.preventDefault();
-          const text = e.target.elements.text.value;
-          const importance = e.target.elements.importance.value;
-          if (text.trim() !== '') {
-            this.addTodo(text, importance);
-            e.target.reset();
-          }
-        }}>
-        <input type="text" name="text" placeholder="Add a new todo item" />
-          <select name="importance">
-            <option value="low">Low</option>
-            <option value="medium">Medium</option>
-            <option value="high">High</option>
-            <option value="critical">Critical</option>
-          </select>
-          <button type="submit">Add</button>
-        </form>
-      </div>
-    );
-  }
+                <form id='new-todo-form' onSubmit={e => {
+                    e.preventDefault();
+                    const text = e.target.elements.text.value;
+                    const importance = e.target.elements.importance.value;
+                    if (text.trim() !== '') {
+                        this.addTodo(text, importance);
+                        e.target.reset();
+                    }
+                }}>
+                    <input id='new-todo-input' type="text" name="text" placeholder="Add a new todo item" />
+                    <select id='importance-selector' name="importance">
+                        <option id='low-importance-option' className='importance-selector-option' value="low">Low</option>
+                        <option id='medium-importance-option' className='importance-selector-option' value="medium">Medium</option>
+                        <option id='high-importance-option' className='importance-selector-option' value="high">High</option>
+                        <option id='critical-importance-option' className='importance-selector-option' value="critical">Critical</option>
+                    </select>
+                    <button id='add-new-todo-btn' type="submit">Add</button>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default TodoList;
